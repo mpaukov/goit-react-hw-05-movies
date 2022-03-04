8a9f0305f816e7314f741b79549cfb3f9a58b74a
Michael Paukov <michael.paukov@gmail.com>
2022-03-04 11:08:04 +0100

    trying

--- src/components/pages/MovieDetailsPage/MovieDetails.styled.jsx
+++ src/components/pages/MovieDetailsPage/MovieDetails.styled.jsx
@@ -1,7 +1,23 @@
 import styled from '@emotion/styled';
 
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
+export const ImageThumb = styled.div`
+  margin-right: 50px;
+`;
+
 export const Image = styled.img`
   display: block;
   max-width: 100%;
   height: auto;
+`;
+
+export const Description = styled.div`
+  display: flex;
+`;
+
+export const Title = styled.h2`
+  font-weight: 700;
 `;
--- src/components/pages/MovieDetailsPage/MovieDetailsPage.js
+++ src/components/pages/MovieDetailsPage/MovieDetailsPage.js
@@ -2,11 +2,18 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import * as movieAPI from '../../../services/movie-api';
 
-import { Image } from './MovieDetails.styled.jsx';
+import { Image, Wrapper } from './MovieDetails.styled';
 import placeholderImg from '../../../images/placeholder.bmp';
+import { Description } from './MovieDetails.styled';
+import { Title } from './MovieDetails.styled';
+import { ImageThumb } from './MovieDetails.styled';
 
 export function MovieDetailsPage() {
   const [data, setData] = useState(null);
+  console.log(
+    '🚀 ~ file: MovieDetailsPage.js ~ line 13 ~ MovieDetailsPage ~ data',
+    data
+  );
 
   const { movieId } = useParams();
 
@@ -17,15 +24,22 @@ export function MovieDetailsPage() {
   return (
     <>
       {data && (
-        <Image
-          src={
-            data.poster_path
-              ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
-              : placeholderImg
-          }
-          alt={data.original_title}
-          loading="lazy"
-        />
+        <Wrapper>
+          <ImageThumb>
+            <Image
+              src={
+                data.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
+                  : placeholderImg
+              }
+              alt={data.original_title}
+              loading="lazy"
+            />
+          </ImageThumb>
+          <Description>
+            <Title>{data.original_title}</Title>
+          </Description>
+        </Wrapper>
       )}
     </>
   );
